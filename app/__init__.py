c42e66eb60c60817589c7ae5cea055fcb0bd52fd
rayturner377 <turnerray377@gmail.com>
2020-04-12 12:28:09 -0500

    Final commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://eelxygymmyyfol:0d72aada4bce21ebb6347e6ad6d09b5abfc7f2d1a43c051d22730c7cd92412d3@ec2-18-215-99-63.compute-1.amazonaws.com:5432/d1in0i6b8fj806"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lbmkizyoaccchu:a0c8fa193e9da2213959d93f5a5f3e415b9bf85eec418585c4ac3f7e85c6158b@ec2-34-193-232-231.compute-1.amazonaws.com:5432/d4jp4rhid4o5m6"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
